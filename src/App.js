6be855776357515417be90b42eebf6030a5e9ef8
Laura Ku <lauraku888@gmail.com>
2023-07-04 16:21:17 -0400

    test grizzly api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { client } from "@gradio/client"
+
+async function run() {
+
+  const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
+  const exampleImage = await response_0.blob();
+
+  const app = await client("https://singularnostalgia-grizzle.hf.space/");
+  const result = await app.predict("/predict", [
+    exampleImage, 	// blob in 'img' Image component
+  ]);
+
+  console.log(result?.data);
+}
 
 function App() {
+  useEffect(() => {
+    run()
+  })
+
   return (
     <div>
       <h1>Welcome to Some Grizzly Demo</h1>
