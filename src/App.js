c2a0548541289623cfe3512160cb521bfbc32ea4
Laura Ku <lauraku888@gmail.com>
2023-07-04 15:40:44 -0400

    initial static site setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Some Grizzly Demo</h1>
+      <p>This is a static React site hosted on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App
